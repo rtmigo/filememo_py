c88507cd633d8a531fe13b0af1661e716b9ffd0b
rtmigo <art.galkin@gmail.com>
2021-05-15 17:34:15 +0300

    tests for persistence

---
+++ tests/keeps_value/run_me_twice.py
@@ -0,0 +1,41 @@
+import sys
+from pathlib import Path
+from filememo import memoize
+
+cache_path = Path(__file__).parent / "_cachedir"
+
+
+def increase_value_in_file(basename: str):
+    file = (Path(__file__).parent / basename)
+    try:
+        t = file.read_text()
+    except FileNotFoundError:
+        t = '0'
+
+    file.write_text(str(int(t) + 1))
+
+
+@memoize(dir_path=cache_path)
+def cached():
+    increase_value_in_file('_memoized.txt')
+
+
+def non_cached():
+    increase_value_in_file('_non_memoized.txt')
+
+
+@memoize(version=sys.argv[2] if len(sys.argv) >= 3 else None)
+def cached_systemp():
+    increase_value_in_file('_memoized_systemp.txt')
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "memoized":
+        cached()
+    elif sys.argv[1] == "non_memoized":
+        non_cached()
+    elif sys.argv[1] == "systemp":
+        cached_systemp()
+
+    else:
+        raise ValueError
