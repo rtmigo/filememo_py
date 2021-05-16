54a3834389b9542fb335bfc1991eb8f2c2517f75
rtmigo <art.galkin@gmail.com>
2021-05-16 18:21:42 +0300

    pkg test

--- test_pkg.sh
+++ test_pkg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e && source pyrel.sh
+set -e
+
+wget -O /tmp/pyrel.sh https://raw.githubusercontent.com/rtmigo/pyrel/master/pyrel.sh
+source /tmp/pyrel.sh
 
 # build package, install it into virtual
 # environment with pip
@@ -7,7 +10,7 @@ pyrel_test_begin
 
 # check, that we can import this module by name
 # (so it's installed)
-python3 -c "import pickledir"
+python3 -c "import filememo"
 
 # remove generated package
 pyrel_test_end
