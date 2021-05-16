44ffe3b68ed0dd8c393dae9efa945d3f59e2bb34
rtmigo <art.galkin@gmail.com>
2021-05-16 03:07:46 +0300

    exceptions

--- filememo/_deco.py
+++ filememo/_deco.py
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: MIT
 
 
-import datetime
+import datetime as dt
 import functools
 import hashlib
 import tempfile
 from pathlib import Path
-from typing import Callable, Union
+from typing import Callable, Union, Optional
 
 from pickledir import PickleDir
 
@@ -18,16 +18,27 @@ def _md5(s: str):
     return hashlib.md5(s.encode('utf-8')).hexdigest()
 
 
+class FunctionException(BaseException):
+    def __init__(self, inner: BaseException):
+        self.inner = inner
+
+
+def _utc() -> dt.datetime:
+    return dt.datetime.utcnow().replace(tzinfo=dt.timezone.utc)
+
+
 def memoize(function: Callable = None,
             dir_path: Union[Path, str] = None,
-            max_age: datetime.timedelta = None,
+            max_age: dt.timedelta = dt.timedelta.max,
+            exceptions_max_age: Optional[dt.timedelta] = dt.timedelta.max,
             version: int = None) -> Callable:
     # If called without method, we've been called with optional arguments.
     # We return a decorator with the optional arguments filled in.
     # Next time round we'll be decorating method.
     if function is None:
         return functools.partial(memoize, dir_path=dir_path, max_age=max_age,
-                                 version=version)
+                                 version=version,
+                                 exceptions_max_age=exceptions_max_age)
 
     # method_str = _file_and_method(method)
 
@@ -36,20 +47,56 @@ def memoize(function: Callable = None,
         key = (args, kwargs)
 
         # try get existing result from the cache
-        try:
-            # we will use max_age on both reading and writing
-            result = f.data.get(key, max_age=max_age, default=KeyError)
-            assert result != KeyError  # KeyError will be raised, not returned
-            return result
-        except KeyError:
-            pass
+        # try:
+        # we will use max_age on both reading and writing
+
+        # print(f.data.dirpath)
+
+        pd_result_max_age = max_age if max_age != dt.timedelta.max else None
+
+        record = f.data._get_record(key, max_age=pd_result_max_age)
+        if record is not None:
+
+            exception, result = record.data
+
+            if not exception:
+                assert result != KeyError  # KeyError will be raised, not returned
+                return result
+
+            assert exception is not None
+            if exceptions_max_age == dt.timedelta.max or \
+                    (_utc() - record.created) <= exceptions_max_age:
+                raise FunctionException(exception)
+            else:
+                # we did not return result and did not raise exception.
+                # We will just restart the function
+                pass
 
         # get new result and store it to the cache
-        new_result = function(*args, **kwargs)
-        # we will use max_age on both reading and writing
-        f.data.set(key, max_age=max_age, value=new_result)
+        exception: Optional[BaseException] = None
+        result = None
+        try:
+            result = function(*args, **kwargs)
+        except BaseException as error:
+            if not exceptions_max_age:
+                raise FunctionException(exception)
+            exception = error
 
-        return new_result
+        assert exception is None or exceptions_max_age is not None
+
+        pd_exception_max_age = (exceptions_max_age
+                                if exceptions_max_age != dt.timedelta.max
+                                else None)
+
+        # we will use max_age on both reading and writing
+        f.data.set(key,
+                   max_age=pd_exception_max_age if exception else pd_result_max_age,
+                   value=(exception, result))
+
+        if exception:
+            raise FunctionException(exception)
+
+        return result
 
     # dir_path is the parent path. Within this directory, we will create
     # a subdirectory that uniquely matches the function we are decorating.
@@ -61,7 +108,7 @@ def memoize(function: Callable = None,
     if dir_path is not None:
         func_parent_dir = Path(dir_path)
     else:
-        func_parent_dir = Path(tempfile.gettempdir())/'filememo'
+        func_parent_dir = Path(tempfile.gettempdir()) / 'filememo'
 
     func_cache_dir = find_dir_for_method(func_parent_dir, function)
 
